c64fdb3ce5597e28352fc698e74b12d030bf3225
Anam Hira <hira.anam49@gmail.com>
2023-08-16 04:13:40 +0000

    test web and test seed training

---
+++ onlineLearning/test_seed_train.py
@@ -0,0 +1,109 @@
+from onlineLearning.feedback import Framework, OurArguments
+from onlineLearning.tasks import get_task
+import torch
+import numpy as np
+improt random
+import argparse
+import tasks
+from transformers import AutoConfig, AutoTokenizer, AutoModelForCausalLM, Trainer, HfArgumentParser, Trainer, TrainingArguments, DataCollatorWithPadding, DataCollatorForTokenClassification
+from utils import *
+from trainer import OurTrainer
+import random
+'''
+
+Seed training class for initial training of mind2web to get the intial loss going
+
+'''
+
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser = HfArgumentParser(OurArguments)
+    args = parser.parse_args_into_dataclasses()[0]
+    print(args)
+    return args
+
+def result_file_tag(args):
+    """
+    Get the result file tag
+    """
+    save_model_name = args.model_name.split("/")[-1]
+    sfc_tag = "-sfc" if args.sfc else ""
+    icl_sfc_tag = "-icl_sfc" if args.icl_sfc else ""
+    sample_eval_tag = "-sampleeval%d" % args.num_eval if args.num_eval is not None else ""
+    sample_train_tag = "-ntrain%d" % args.num_train if args.num_train > 0 else ""
+    sample_dev_tag = "-ndev%d" % args.num_dev if args.num_dev is not None else ""
+    customized_tag = f"-{args.tag}" if len(args.tag) > 0 else ""
+    return f"{args.task_name}-{save_model_name}" + sfc_tag + icl_sfc_tag + sample_eval_tag + sample_train_tag + sample_dev_tag + customized_tag
+
+
+def main():
+    args = parse_args()
+
+    set_seed(args.seed)
+    task = get_task(args.task_name)
+    train_sets = task.sample_train_sets(num_train=args.num_train, num_dev=args.num_dev, num_eval=args.num_eval, num_train_sets=args.num_train_sets, seed=args.train_set_seed)
+
+    # Initialize trainer and load model
+    framework = Framework(args, task)
+
+    if args.train_set_seed is not None or args.num_train_sets is not None:
+        # Eval samples share one (or multiple) training set(s)
+        for train_set_id, train_samples in enumerate(train_sets):
+            train_set_seed = train_set_id if args.train_set_seed is None else args.train_set_seed
+
+            # Sample eval samples
+            if args.num_eval is not None:
+                eval_samples = task.sample_subset(data_split="valid", seed=train_set_seed, num=args.num_eval)
+            else:
+                eval_samples = task.valid_samples
+
+            if args.trainer != "none":
+                if args.num_dev is not None:
+                    # Dev samples
+                    dev_samples = train_samples[-args.num_dev:] 
+                    train_samples = train_samples[:-args.num_dev]
+                else:
+                    dev_samples = None
+
+                # Training
+                framework.train(train_samples, dev_samples if dev_samples is not None else eval_samples)
+
+                if not args.no_eval:
+                    metrics = framework.evaluate([], eval_samples) # No in-context learning if there is training
+                    if dev_samples is not None:
+                        dev_metrics = framework.evaluate([], dev_samples) 
+                        for m in dev_metrics:
+                            metrics["dev_" + m] = dev_metrics[m]
+            else:
+                assert args.num_dev is None
+                # Zero-shot / in-context learning
+                metrics = framework.evaluate(train_samples, eval_samples)
+
+            if not args.no_eval:
+                logger.info("===== Train set %d =====" % train_set_seed)
+                logger.info(metrics)
+                if args.local_rank <= 0:
+                    write_metrics_to_file(metrics, "result/" +  result_file_tag(args) + f"-trainset{train_set_id}.json" if args.result_file is None else args.result_file)
+
+    else:
+        # For each eval sample, there is a training set. no training is allowed
+        # This is for in-context learning (ICL)
+        assert args.trainer == "none"
+        if args.num_eval is not None:
+            eval_samples = task.sample_subset(data_split="valid", seed=0, num=args.num_eval)
+        else:
+            eval_samples = task.valid_samples
+
+        metrics = framework.evaluate(train_sets, eval_samples, one_train_set_per_eval_sample=True)
+        logger.info(metrics)
+        if args.local_rank <= 0:
+            write_metrics_to_file(metrics, "result/" + result_file_tag(args) + "-onetrainpereval.json" if args.result_file is None else args.result_file)
+
+if __name__ == "__main__": 
+    main()
